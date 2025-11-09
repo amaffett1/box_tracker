7cf224b7481b92118d56b18a8b132dd9f1910409
alessandro <maffettonealessandro@gmail.com>
2025-11-09 11:13:05 +0100

    Update Puma port from 9292 to 3000 for production setup

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,7 @@
 max_threads_count = ENV.fetch(" RAILS_MAX_THREADS") {  5 }min_threads_count = ENV.fetch("R AILS_MIN_THREADS") { m ax_threads_count }
 threads min _threads_count, max_threads_count
 
-port ENV.f etch("PORT ") { 9292 }
+port ENV.f etch("PORT ") { 3000 }
 environment ENV.fe tch("RAILS _ENV") { "prod uction" }
 pidfile ENV.fet ch("PIDFIL E") { "tmp/p ids/server.pid" }
 workers ENV.fetch ("WEB_CONC URRENCY") { 2 }
