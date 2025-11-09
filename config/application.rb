1ed1887a242a1ee694cba2f94f76158156ee078e
alessandro <maffettonealessandro@gmail.com>
2025-11-09 15:01:59 +0100

    aggiornati application.rb, cable.yml, creato active_job.yml, database.yml

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,13 @@ module BoxTracker
     config.load_defaults 8.1
 
     config.encoding = "utf-8"
+    # 🔹 Usa PostgreSQL per Active Record
+    config.active_record.schema_format = :sql
+
+    # 🔹 Usa ActiveJob async (no database per queue)
+    config.active_job.queue_adapter = :async
+
+    # 🔹 Prepara eventuali WebSocket, ma senza Redis
+    config.action_cable.disable_request_forgery_protection = true
   end
 end
